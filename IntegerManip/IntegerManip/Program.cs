6e1c5b8f8fa389407fa8cf17a18cbfd580f710e6
Jacob <Jacob.r.osterberg@gmail.com>
2020-06-17 09:45:44 -0500

    combined Input and menu

--- IntegerManip/IntegerManip/Program.cs
+++ IntegerManip/IntegerManip/Program.cs
@@ -23,9 +23,10 @@ namespace IntegerManip
         static void Main(string[] args)
         
         {
-            ArrayList num = new ArrayList { };
+            List<int> num = new List<int> { };
             int user = 0;
-
+           
+            
             do
             {
                 Console.WriteLine("Please add a number, -1 to exit");
@@ -41,58 +42,59 @@ namespace IntegerManip
                 
             } while (num.Count != 20);
             Console.WriteLine("Input complete");
-
-
-
-            Console.WriteLine("What would you like to see?");
-            Console.WriteLine("1: All numbers entered");
-            Console.WriteLine("2: All even numbers entered");
-            Console.WriteLine("3: All odd numbers entered");
-            Console.WriteLine("4: Every 5th number entered");
-            Console.WriteLine("5: The sum of every number entered");
-            Console.WriteLine("6: Every input thats divisable by 3");
-            Console.WriteLine("7: Quit");
-            string display = Console.ReadLine();
-            int displayNum = Convert.ToInt32(display);
-
-
-
-
-            if (displayNum == 1)
+            int displayNum = 0;
+            do
             {
-                //all numbers
-                for (i = 0; i < num.size(); i++)
-                {
-                    Console.WriteLine(num.get(i));
-                }
-            }
+                Console.WriteLine("What would you like to see?");
+                Console.WriteLine("1: All numbers entered");
+                Console.WriteLine("2: All even numbers entered");
+                Console.WriteLine("3: All odd numbers entered");
+                Console.WriteLine("4: Every 5th number entered");
+                Console.WriteLine("5: The sum of every number entered");
+                Console.WriteLine("6: Every input thats divisable by 3");
+                Console.WriteLine("7: Quit");
+                string display = Console.ReadLine();
+                 displayNum = Convert.ToInt32(display);
 
-            if (displayNum == 2)
-            {
-                //evens
-                for (i = 0; i < num.size(); i++)
+
+
+
+                if (displayNum == 1)
                 {
-                    if (num.get(i) / 2 == 0)
+                    //all numbers
+                    foreach (var i in num)
                     {
-                        Console.WriteLine(num.get(i));
+                        Console.WriteLine(i);
                     }
                 }
-            }
 
-            if (displayNum == 3)
-            {
-                //odds
-                for (i = 0; i < num.size(); i++)
+                if (displayNum == 2)
                 {
-                    if (num.get(i) / 2 != 0)
+                    //evens
+                    foreach (var i in num)
                     {
-                        Console.WriteLine(num.get(i));
+
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
                     }
                 }
-            }
-            //Add in evry other comand in ifs
 
+                if (displayNum == 3)
+                {
+                    //odds
+                    foreach (var i in num)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+                //Add in evry other comand in ifs
 
+            } while (displayNum != 7);
             Console.ReadLine();
         }
     }
