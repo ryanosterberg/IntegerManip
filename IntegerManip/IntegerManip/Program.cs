4edc7c17e6dfdca0e4ff788bd5204bfaf9b2e09a
Rebecca Gilpatric <rebeccagilpatric@gmail.com>
2020-06-17 09:57:49 -0500

    Merge remote-tracking branch 'origin/dev' into sumAndEveryFifthRebecca

--- IntegerManip/IntegerManip/Program.cs
+++ IntegerManip/IntegerManip/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,14 +21,80 @@ namespace IntegerManip
         //• Display every number divisible by 3
         //• Quit
         static void Main(string[] args)
+        
         {
+            List<int> num = new List<int> { };
+            int user = 0;
+           
+            
+            do
+            {
+                Console.WriteLine("Please add a number, -1 to exit");
+                user = Convert.ToInt32(Console.ReadLine());
+                
+                if (user != -1)
+                {
+                    num.Add(user);
+                } else
+                {
+                    break;
+                }
+                
+            } while (num.Count != 20);
+            Console.WriteLine("Input complete");
+            int displayNum = 0;
+            do
+            {
+                Console.WriteLine("What would you like to see?");
+                Console.WriteLine("1: All numbers entered");
+                Console.WriteLine("2: All even numbers entered");
+                Console.WriteLine("3: All odd numbers entered");
+                Console.WriteLine("4: Every 5th number entered");
+                Console.WriteLine("5: The sum of every number entered");
+                Console.WriteLine("6: Every input thats divisable by 3");
+                Console.WriteLine("7: Quit");
+                string display = Console.ReadLine();
+                 displayNum = Convert.ToInt32(display);
 
-                //these vars should be there already so this code block should be deleted when merged and variable names changed
-                int displayNum =4;//will be the number corresponding with menu
-                ArrayList num = new ArrayList();
-                num.Add(1); num.Add(2); num.Add(3); num.Add(4); num.Add(5); num.Add(6); num.Add(7); num.Add(8); num.Add(9); num.Add(10); num.Add(11);
 
 
+
+                if (displayNum == 1)
+                {
+                    //all numbers
+                    foreach (var i in num)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (displayNum == 2)
+                {
+                    //evens
+                    foreach (var i in num)
+                    {
+
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+
+                if (displayNum == 3)
+                {
+                    //odds
+                    foreach (var i in num)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+                //Add in evry other comand in ifs
+
+            
             if (displayNum == 3)//every fifth number
             {
                 for(int i=0; i<num.Count; i++)//for each index
@@ -48,8 +116,7 @@ namespace IntegerManip
                 }
                 Console.WriteLine("The sum is " + sum);
             }
-
-
+            } while (displayNum != 7);
             Console.ReadLine();
         }
     }
