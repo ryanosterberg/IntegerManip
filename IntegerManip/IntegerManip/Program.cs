aa3d017a40b4a10c97ce9eed698722a242d04795
Rebecca Gilpatric <rebeccagilpatric@gmail.com>
2020-06-16 20:42:38 -0500

    Initial sum and everyfifth -- Needs changes

--- IntegerManip/IntegerManip/Program.cs
+++ IntegerManip/IntegerManip/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,36 @@ namespace IntegerManip
         //• Quit
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
+
+                //these vars should be there already so this code block should be deleted when merged and variable names changed
+                int menuItem =4;//will be the number corresponding with menu
+                ArrayList num = new ArrayList();
+                num.Add(1); num.Add(2); num.Add(3); num.Add(4); num.Add(5); num.Add(6); num.Add(7); num.Add(8); num.Add(9); num.Add(10); num.Add(11);
+
+
+            if (menuItem == 3)//every fifth number
+            {
+                for(int i=0; i<num.Count; i++)//for each index
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.Write(num[i] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            if(menuItem == 4)//sum
+            {
+                int sum = 0;
+                foreach(int i in num)
+                {
+                    sum = sum + i;
+                }
+                Console.WriteLine("The sum is " + sum);
+            }
+
+
             Console.ReadLine();
         }
     }
