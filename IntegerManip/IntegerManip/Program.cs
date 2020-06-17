165d857798c4f9d2fd4cd2b35e42b7e56e29f1e7
Artie Krishnan <artiekrishnan@gmail.com>
2020-06-17 08:47:12 -0500

    Added If Loop

--- IntegerManip/IntegerManip/Program.cs
+++ IntegerManip/IntegerManip/Program.cs
@@ -10,19 +10,25 @@ namespace IntegerManip
     {
         //As a team, write a project to prompt the user for up to 20 integers.  
         //The user will be allowed to choose from the following menu options until they decide to quit:
-        //• Display the whole list of input
-        //• Display every even number
-        //• Display every odd number
-        //• Display every 5th number
-        //• Display the sum of the integers
-        //• Display every number divisible by 3
-        //• Quit
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
-        }
+        //• 0 Display the whole list of input Jacob
+        //• 1 Display every even number Jacob
+        //• 2 Display every odd number Jacob
+        //• 3 Display every 5th number
+        //• 4 Display the sum of the integers Rebecca
+        //• 5 Display every number divisible by 3 - Artie
+        //• 6 Quit
+ 
+           if( input == 5)
+            {
+                for(int p = 0; p < num.size(); p++)
+                {
+                    int div = num.get(p);
+                    if((a % 3) == 0)
+                    {
+                        Console.WriteLine(a);
+                    }
+                }
+            }
+        
     }
 }
