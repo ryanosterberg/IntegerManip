ffca49d94faefd1128f46a992964d618c15ecfda
Jacob <Jacob.r.osterberg@gmail.com>
2020-06-16 21:18:32 -0500

    Menu and my outputs

--- IntegerManip/IntegerManip/Program.cs
+++ IntegerManip/IntegerManip/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,9 +20,59 @@ namespace IntegerManip
         //• Quit
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
+
+
+            Console.WriteLine("What would you like to see?");
+            Console.WriteLine("1: All numbers entered");
+            Console.WriteLine("2: All even numbers entered");
+            Console.WriteLine("3: All odd numbers entered");
+            Console.WriteLine("4: Every 5th number entered");
+            Console.WriteLine("5: The sum of every number entered");
+            Console.WriteLine("6: Every input thats divisable by 3");
+            Console.WriteLine("7: Quit");
+            string display = Console.ReadLine();
+            int displayNum = Convert.ToInt32(display);
+
+
+
+
+            if (displayNum == 1)
+            {
+                //all numbers
+                for (i = 0; i < num.size(); i++)
+                {
+                    Console.WriteLine(num.get(i));
+                }
+            }
+            else
+            {
+                if (displayNum == 2)
+                {
+                    //evens
+                    for (i = 0; i < num.size(); i++)
+                    {
+                        if (num.get(i) / 2 == 0)
+                        {
+                            Console.WriteLine(num.get(i));
+                        }
+                    }
+                }
+                else
+                {
+                    if (displayNum == 3)
+                    {
+                        //odds
+                        for (i = 0; i < num.size(); i++)
+                        {
+                            if (num.get(i) / 2 != 0)
+                            {
+                                Console.WriteLine(num.get(i));
+                            }
+                        }
+                    }
+                    //Add in evry other comand in elses and ifs
+                }
+            }
             Console.ReadLine();
         }
     }
